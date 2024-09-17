61178ddaebdb87f80835596c35735e027512c8c5
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-17 16:37:50 +0300

    update interface_start.py

--- Start/Ch 2/interface_start.py
+++ Start/Ch 2/interface_start.py
@@ -11,15 +11,25 @@ class GraphicShape(ABC):
     @abstractmethod
     def calcArea(self):
         pass
+  
+    
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
 
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
+    
+    def toJSON(self):
+        return f"{{ \"square\": {str(self.calcArea())} }}"
 
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
