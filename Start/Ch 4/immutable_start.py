219131bbc131c193859aac1235cb08a422e592f0
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-19 02:30:13 +0300

    update immutable_start.py

--- Start/Ch 4/immutable_start.py
+++ Start/Ch 4/immutable_start.py
@@ -8,12 +8,20 @@ from dataclasses import dataclass
 class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
+    
+    def somefunc(self, newval):
+        self.value2 = newval
 
 
 obj = ImmutableClass()
 print(obj.value1)
 
 # TODO: attempting to change the value of an immutable class throws an exception
-
+obj2 = ImmutableClass("Another Value", 15)
+print(obj2)
 
 # TODO: even functions within the class can't change anything
+obj.value1 = "Another value"
+print(obj.value1)
+
+obj.somefunc(20)
